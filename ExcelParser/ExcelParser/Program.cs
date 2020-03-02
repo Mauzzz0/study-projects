48fb89942aef1ee7e0fd4f3e68f6201bc6cb0584
Ruslan <ruslan.semack1501@gmail.com>
2020-03-02 21:50:41 +0300

    tmp1

--- ExcelParser/ExcelParser/Program.cs
+++ ExcelParser/ExcelParser/Program.cs
@@ -11,7 +11,17 @@ namespace ExcelParser
     {
         static void Main(string[] args)
         {
+            List<string> test1 = new List<string> { };
+            test1.Add("1");
+            test1.Add("2");
+            test1.Add("3");
 
+
+            Console.WriteLine(test1[0]);
+            Console.WriteLine(test1[1]);
+            Console.WriteLine(test1[2]);
+            //Console.WriteLine(test1[3]);
+            Console.ReadLine();
         }
     }
 }
