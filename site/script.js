67720109f5381302a8f8e95cf39349cd23b21927
Neervel <klihopoy@mail.ru>
2020-03-06 21:26:31 +0300

    auto set dates

--- site/script.js
+++ site/script.js
@@ -1,56 +1,81 @@
 function setDates(date){
+    
     var date = new Date(),
     targetDay = 1, 
     targetDate = new Date(),
     delta = targetDay - date.getDay();
     
-    if (delta >= 0) {
-        targetDate.setDate(date.getDate() - delta)
-    }
+    if (delta >= 0) {targetDate.setDate(date.getDate() - delta)}
     
-    else {
-        targetDate.setDate(date.getDate() + delta)
-    }
-    
-    for(let i=0; i<6; i++){
-    targetDate.setDate(targetDate.getDate()+i);
+    else {targetDate.setDate(date.getDate() + delta)}
     
     let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-    var month = months[targetDate.getMonth()];
     let w_days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-    var w_day = w_days[targetDate.getDay()];
     let days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
-    var day = days[targetDate.getDate()-1]
     t_year = targetDate.getFullYear();
     let a = String(t_year)[2];
     let b = String(t_year)[3];
+    
+    var month = months[targetDate.getMonth()];
+    var w_day = w_days[targetDate.getDay()];
+    var day = days[targetDate.getDate()-1]
     let year = a+b;
     var res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_"+1).innerHTML = res;
-    }
+    document.getElementById("day_1").innerHTML = res;
     
-    return res;
-    alert(res);
+    targetDate.setDate(targetDate.getDate()+1);
+    month = months[targetDate.getMonth()];
+    w_day = w_days[targetDate.getDay()];
+    day = days[targetDate.getDate()-1]
+    year = a+b;
+    res = w_day+" "+"("+day+"."+month+"."+year+")";
+    document.getElementById("day_2").innerHTML = res;
+    
+    targetDate.setDate(targetDate.getDate()+1);
+    month = months[targetDate.getMonth()];
+    w_day = w_days[targetDate.getDay()];
+    day = days[targetDate.getDate()-1]
+    year = a+b;
+    res = w_day+" "+"("+day+"."+month+"."+year+")";
+    document.getElementById("day_3").innerHTML = res;
+    
+    targetDate.setDate(targetDate.getDate()+1);
+    month = months[targetDate.getMonth()];
+    w_day = w_days[targetDate.getDay()];
+    day = days[targetDate.getDate()-1]
+    year = a+b;
+    res = w_day+" "+"("+day+"."+month+"."+year+")";
+    document.getElementById("day_4").innerHTML = res;
+    
+    targetDate.setDate(targetDate.getDate()+1);
+    month = months[targetDate.getMonth()];
+    w_day = w_days[targetDate.getDay()];
+    day = days[targetDate.getDate()-1]
+    year = a+b;
+    res = w_day+" "+"("+day+"."+month+"."+year+")";
+    document.getElementById("day_5").innerHTML = res;
+    
+    targetDate.setDate(targetDate.getDate()+1);
+    month = months[targetDate.getMonth()];
+    w_day = w_days[targetDate.getDay()];
+    day = days[targetDate.getDate()-1]
+    year = a+b;
+    res = w_day+" "+"("+day+"."+month+"."+year+")";
+    document.getElementById("day_6").innerHTML = res;
+    
 }
 
-function test(key, a) {
-        if (key == "Enter"){
-            text = a.value.toUpperCase();
-            
-            if(text.length != 10){
-                alert("Введите группу!");
-                a.value = "";
-            } 
-         else{
-               setDates(new Date());
-               //document.getElementById("day_1").innerHTML = setDates(new Date());
-               //document.getElementById("day_2").innerHTML = setDates(new Date());
-               //document.getElementById("day_3").innerHTML = setDates(new Date());
-               //document.getElementById("day_4").innerHTML = setDates(new Date());
-               //document.getElementById("day_5").innerHTML = setDates(new Date());
-               //document.getElementById("day_6").innerHTML = setDates(new Date());
-               a.value = "";
-               }
+function load(key, a) {
+    if (key == "Enter"){
+        text = a.value.toUpperCase();
+        if(text.length != 10){
+            alert("Введите группу!");
+            a.value = "";
+        } 
+        else{
+            a.value = "";
+            setDates(new Date());
         }
+    }
 }
     
