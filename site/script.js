96796dec817cc80f2364bca1ca09f72c39cbed54
Neervel <klihopoy@mail.ru>
2020-03-17 22:52:04 +0300

    починил себе баш

--- site/script.js
+++ site/script.js
@@ -5,6 +5,7 @@ function setDates(){
     targetDay = 1, 
     targetDate = new Date();
     var delta = targetDay - date.getDay();
+    
     if (delta > 0) {targetDate.setDate(date.getDate()-6)}
     
     else {
