8d6cef827b840ddd23229fa2b361048850b5bda1
Neervel <klihopoy@mail.ru>
2020-03-21 18:56:08 +0300

    final date set

--- site/script.js
+++ site/script.js
@@ -1,238 +1,117 @@
-var weeks = 0;
+let weeks = 0,
+    month_delta,
+    year_delta,
+    t_year, a, b, month, w_day, day, year, res;
 
-function setDates(){
-    var date = new Date(),
-    targetDay = 1, 
-    targetDate = new Date();
-    var delta = targetDay - date.getDay();
-    
-    if (delta > 0) {targetDate.setDate(date.getDate()-6)}
-    
+const today = new Date();
+
+const selGroupInput = document.querySelector(".input_group"),
+    nextWeek = document.querySelector('.next_week'),
+    previousWeek = document.querySelector('.previous_week'),
+    logo = document.querySelector('.logo');
+
+const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+
+    w_days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+
+    days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+         '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', 
+            '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+
+function setDates(date) {
+
+    targetDay = 1,
+        targetDate = new Date();
+
+    if (targetDate.getYear() < date.getYear()) {
+        year_delta = date.getYear() - targetDate.getYear();
+        targetDate.add(1 * year_delta).year();
+
+    } else if (targetDate.getYear() > date.getYear()) {
+        year_delta = targetDate.getYear() - date.getYear();
+        targetDate.add(-1 * year_delta).year();
+    }
+
+    if (targetDate.getMonth() < date.getMonth()) {
+        month_delta = date.getMonth() - targetDate.getMonth();
+        targetDate.add(1 * month_delta).month();
+
+    } else if (targetDate.getMonth() > date.getMonth()) {
+        month_delta = targetDate.getMonth() - date.getMonth();
+        targetDate.add(-1 * month_delta).month();
+    }
+
+    let delta = targetDay - date.getDay();
+
+    if (delta > 0) {
+        targetDate.setDate(date.getDate() + 1)
+    } 
     else {
         targetDate.setDate(date.getDate() + delta);
-        document.getElementById("day_"+date.getDay()).style.backgroundImage = "linear-gradient(-60deg, #ff5858 0%, #f09819 100%)";
+        if (date.getDate() === (new Date()).getDate()) {
+            document.getElementById("day_" + today.getDay()).style.backgroundImage = "linear-gradient(-60deg, #ff5858 0%, #f09819 100%)";
+        } else {
+            document.getElementById("day_" + today.getDay()).removeAttribute('style');
+        }
     }
-    
-    const months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-    const w_days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-    const days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
-    
-    var t_year = targetDate.getFullYear();
-    let a = String(t_year)[2];
-    let b = String(t_year)[3];
-    
-    var month = months[targetDate.getMonth()];
-    var w_day = w_days[targetDate.getDay()];
-    var day = days[targetDate.getDate()-1]
-    let year = a+b;
-    var res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_1").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_2").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_3").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_4").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_5").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_6").innerHTML = res;
-    
-    
+
+    for (let i = 1; i < 7; i++) {
+        t_year = targetDate.getFullYear();
+        a = String(t_year)[2];
+        b = String(t_year)[3];
+        month = months[targetDate.getMonth()];
+        w_day = w_days[targetDate.getDay()];
+        day = days[targetDate.getDate() - 1]
+        year = a + b;
+        res = w_day + " " + "(" + day + "." + month + "." + year + ")";
+        document.getElementById("day_" + i).innerHTML = res;
+        targetDate.setDate(targetDate.getDate() + 1);
+    }
 }
 
 function load(key, a) {
-    text = a.value.toUpperCase();
-    if (key == "Enter"){
-        if(text.length != 10){
-            a.value="";
+    const text = a.value.toUpperCase();
+    if (key == "Enter") {
+        if (text.length != 10) {
+            a.value = "";
             alert("Введите группу!");
-            
-        } 
-        else{
-            alert("Расписание для группы: "+text);
+        } else {
+            console.log("Расписание для группы: " + text);
             document.getElementById('now_week').scrollIntoView();
         }
-    }
+    } else {}
 }
 
 
- 
-function set_next_week(){
-    weeks+=1;
-    var date = new Date();
-    date.setDate(date.getTime()+(86400000*7*weeks));
-    var targetDay = 1, 
-    targetDate = new Date(),
-    delta = targetDay - date.getDay();
-    
-    if(delta=1){
-        targetDate.setDate(date.getDate()+1)
-    }
-    
-    else{
-    if (delta >= 0) {targetDate.setDate(date.getDate() - delta)}
-    
-    
-    else {targetDate.setDate(date.getDate() + delta)}
-    }
-    
-    let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-    let w_days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-    let days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
-    t_year = targetDate.getFullYear();
-    let a = String(t_year)[2];
-    let b = String(t_year)[3];
-    
-    var month = months[targetDate.getMonth()];
-    var w_day = w_days[targetDate.getDay()];
-    var day = days[targetDate.getDate()-1]
-    let year = a+b;
-    var res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_1").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_2").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_3").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_4").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_5").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_6").innerHTML = res;
-    
-}
+document.addEventListener("DOMContentLoaded", () => {
+    let date = new Date();
+    setDates(date, 'y');
+});
 
-function set_previous_week(){
-    weeks-=1;
-    var date = new Date();
-    date.setDate(date.Date()+(7*weeks));
-    var targetDay = 1, 
-    targetDate = new Date(),
-    delta = targetDay - date.getDay();
-    
-    if(delta=1){
-        targetDate.setDate(date.getDate()+1)
+selGroupInput.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
     }
-    
-    else{
-    if (delta >= 0) {targetDate.setDate(date.getDate() - delta)}
-    
-    
-    else {targetDate.setDate(date.getDate() + delta)}
-    }
-    
-    let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-    let w_days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-    let days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
-    t_year = targetDate.getFullYear();
-    let a = String(t_year)[2];
-    let b = String(t_year)[3];
-    
-    var month = months[targetDate.getMonth()];
-    var w_day = w_days[targetDate.getDay()];
-    var day = days[targetDate.getDate()-1]
-    let year = a+b;
-    var res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_1").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_2").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_3").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_4").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_5").innerHTML = res;
-    
-    targetDate.setDate(targetDate.getDate()+1);
-    month = months[targetDate.getMonth()];
-    w_day = w_days[targetDate.getDay()];
-    day = days[targetDate.getDate()-1]
-    year = a+b;
-    res = w_day+" "+"("+day+"."+month+"."+year+")";
-    document.getElementById("day_6").innerHTML = res;
-}
+    load(event.key, selGroupInput);
+});
+
+nextWeek.addEventListener('click', () => {
+    weeks++;
+    let datePlus = new Date();
+    datePlus.add(7 * weeks).days();
+    setDates(datePlus);
+    console.log(weeks);
+});
+
+previousWeek.addEventListener('click', () => {
+    weeks--;
+    let dateMin = new Date();
+    dateMin.add(7 * weeks).days();
+    setDates(dateMin);
+    console.log(weeks);
+});
+
+logo.addEventListener('click', () => {
+    setDates(new Date());
+    weeks = 0;
+});
