f42d8e129201dbb1b42804f62bca8a22721da51c
Ruslan <ruslan.semack1501@gmail.com>
2020-04-10 23:45:45 +0300

    AssemblyManifest

--- PracticeConsole/Properties/AssemblyInfo.cs
+++ PracticeConsole/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
 [assembly: AssemblyTitle("PracticeConsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("pre-coursework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PracticeConsole")]
+[assembly: AssemblyCompany("Ruslan Semak")]
+[assembly: AssemblyProduct("pre-coursework")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
