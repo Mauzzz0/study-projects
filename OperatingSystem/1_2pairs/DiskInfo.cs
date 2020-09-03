b0ac4713b47a2b99d737c1262dc0cee8f49dfcdb
Ruslan <ruslan.semack1501@gmail.com>
2020-09-04 00:15:43 +0300

    commit

---
+++ OperatingSystem/1_2pairs/DiskInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OperatingSystem._1_2pairs
+{
+    class DiskInfo
+    {
+        static public void fakeMain()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach(DriveInfo drive in drives)
+            {
+                WriteLine($"Name: {drive.Name}");
+                WriteLine($"Type: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    WriteLine($"Size:{drive.TotalSize}");
+                    WriteLine($"FreeSize:{drive.TotalFreeSpace}");
+                    WriteLine($"Label:{drive.VolumeLabel}");
+                }
+                WriteLine();
+            }
+        }
+    }
+}
---
+++ OperatingSystem/1_2pairs/Files.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OperatingSystem._1_2pairs
+{
+    class Files
+    {
+        static public void fakeMain()
+        {
+            string path = @"C:\dirnew\new.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                WriteLine(fileInf.Name);
+                WriteLine(fileInf.CreationTime);
+                WriteLine(fileInf.Length);
+            }
+            /// info
+
+            if (fileInf.Exists) { fileInf.Delete(); }
+            /// delete
+
+            if (fileInf.Exists)
+            {
+                fileInf.MoveTo(@"C:\dirnew1\new.txt");
+            }
+            /// move to
+
+            if (fileInf.Exists)
+            {
+                fileInf.CopyTo(@"C:\dirnew1\new.txt", true);
+            }
+            /// copy
+        }
+    }
+}
