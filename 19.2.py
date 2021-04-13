baad08ed2b85459fc6b4c5dfda2b8f1feeb8bb48
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-04-13 19:53:19 +0300

    complete

--- 19.2.py
+++ 19.2.py
@@ -1,3 +1,31 @@
 def continue_continue_continue_continue_continue_continue_making_sure_the_function_name_is_correct():
     # Function must RETURN values
+    def t2c(A):
+        a = A[:1]
+        b = A[1:]
+        r = int(b)
+        c = 'ABCDEFGH'.find(a) + 1
+        return (c, r)
+
+    def c2t(A):
+        (c, r) = A
+        return 'ABCDEFGH'[c - 1] + str(r)
+
+    def possible_turns(f):
+        (x, y) = t2c(f)
+        to_output = []
+        to_output.append((x + 2, y + 1))
+        to_output.append((x + 2, y - 1))
+        to_output.append((x - 2, y + 1))
+        to_output.append((x - 2, y - 1))
+        to_output.append((x + 1, y + 2))
+        to_output.append((x - 1, y + 2))
+        to_output.append((x + 1, y - 2))
+        to_output.append((x - 1, y - 2))
+        output = []
+        for ((a, b)) in to_output:
+            if (a > 0) & (b > 0) & (a <= 8) & (b <= 8):
+                output += [c2t((a, b))]
+        return sorted(output)
+
 
--- 19.3.py
+++ 19.3.py
@@ -1,2 +1,14 @@
-def continue_continue_continue_continue_continue_continue_continue_making_sure_the_function_name_is_correct():
+import math
+def continue_continue_continue_continue_continue_continue_continue_making_sure_the_function_name_is_correct(a, b, c):
     # Function must RETURN values
+    discr = b ** 2 - 4 * a * c
+    print("Дискриминант D = %.2f" % discr)
+    if discr > 0:
+        x1 = (-b + math.sqrt(discr)) / (2 * a)
+        x2 = (-b - math.sqrt(discr)) / (2 * a)
+        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+    elif discr == 0:
+        x = -b / (2 * a)
+        print("x = %.2f" % x)
+    else:
+        print("Корней нет")
