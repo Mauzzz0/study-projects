57efa7ceec941b413c801f206bee08e3b3845645
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-05-02 12:12:40 +0300

    prac1 full

--- practice1/layouttype/src/main/java/com/mirea/semak/layouttype/MainActivity.java
+++ practice1/layouttype/src/main/java/com/mirea/semak/layouttype/MainActivity.java
@@ -2,7 +2,11 @@ package com.mirea.semak.layouttype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.button14);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
     }
 }
